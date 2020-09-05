6e61776b58dbcb1196fb2c434e9688894dad958a
Adrián Naranjo <adriannaranjosanchez@gmail.com>
2020-09-05 20:05:45 +0200

    VideoRow added, styles App and HomePage changed

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./SearchPage.css";
 import TuneOutlinedIcon from "@material-ui/icons/TuneOutlined";
 import { ChannelRow } from "./ChannelRow";
+import { VideoRow } from "./VideoRow";
 
 export const SearchPage = () => {
   return (
@@ -19,6 +20,61 @@ export const SearchPage = () => {
         subs="660K"
         noOfVideos={382}
         description="Nice description"
+      />
+      <hr />
+      <VideoRow
+        title="How to create a Youtube Clone | 2020"
+        views="2.3M"
+        timestamp="3 days ago"
+        channel="ANaranjoDev"
+        subs="660K"
+        image="https://img-a.udemycdn.com/course/480x270/1551858_d095_2.jpg"
+        description="Nice description for video"
+      />
+      <VideoRow
+        title="How to create a Youtube Clone | 2020"
+        views="2.3M"
+        timestamp="3 days ago"
+        channel="ANaranjoDev"
+        subs="660K"
+        image="https://img-a.udemycdn.com/course/480x270/1551858_d095_2.jpg"
+        description="Nice description for video"
+      />
+      <VideoRow
+        title="How to create a Youtube Clone | 2020"
+        views="2.3M"
+        timestamp="3 days ago"
+        channel="ANaranjoDev"
+        subs="660K"
+        image="https://img-a.udemycdn.com/course/480x270/1551858_d095_2.jpg"
+        description="Nice description for video"
+      />
+      <VideoRow
+        title="How to create a Youtube Clone | 2020"
+        views="2.3M"
+        timestamp="3 days ago"
+        channel="ANaranjoDev"
+        subs="660K"
+        image="https://img-a.udemycdn.com/course/480x270/1551858_d095_2.jpg"
+        description="Nice description for video"
+      />
+      <VideoRow
+        title="How to create a Youtube Clone | 2020"
+        views="2.3M"
+        timestamp="3 days ago"
+        channel="ANaranjoDev"
+        subs="660K"
+        image="https://img-a.udemycdn.com/course/480x270/1551858_d095_2.jpg"
+        description="Nice description for video"
+      />
+      <VideoRow
+        title="How to create a Youtube Clone | 2020"
+        views="2.3M"
+        timestamp="3 days ago"
+        channel="ANaranjoDev"
+        subs="660K"
+        image="https://img-a.udemycdn.com/course/480x270/1551858_d095_2.jpg"
+        description="Nice description for video"
       />
     </div>
   );
